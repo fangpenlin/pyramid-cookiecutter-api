fdaf512c57cb0502e3e93a763f723d6b7b89e352
Fang-Pen Lin <bornstub@gmail.com>
2016-11-19 17:26:57 -0800

    try to fix broken archive script

--- archive.sh
+++ archive.sh
@@ -1,5 +1,7 @@
 git init
 git remote add origin git@github.com:victorlin/pyramid_api.git
+git fetch
+git branch -t master origin/master
 git add --all
 git commit -m "Archive of $CIRCLE_SHA1"
 git push origin master
